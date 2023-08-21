36419b4f0ea827b7394e16cd7f709f83da6d6ceb
ranisup97 <ranisup97@gmail.com>
2023-08-21 09:27:36 +0700

    add footer

--- src/layout/MinimalLayout/Footer/index.tsx
+++ src/layout/MinimalLayout/Footer/index.tsx
@@ -3,12 +3,13 @@ import { Box, Container } from "@mui/system";
 import fbIcon from "../../../assets/images/fbicon.png";
 import twitterIcon from "../../../assets/images/twittericon.png";
 import linkedinIcon from "../../../assets/images/linkedinicon.png";
+import logoImg from "../../../assets/images/logo.png";
 
 const Footer: React.FC = () => {
   const CustomContainer = styled(Container)(({ theme }: { theme: Theme }) => ({
     display: "flex",
-    justifyContent: "space-around",
-    gap: theme.spacing(5),
+    justifyContent: "space-between",
+    // gap: theme.spacing(5),
     [theme.breakpoints.down("sm")]: {
       flexDirection: "column",
       textAlign: "center",
@@ -27,7 +28,7 @@ const Footer: React.FC = () => {
   const FooterLink = styled("span")(({ theme }: { theme: Theme }) => ({
     fontSize: "16px",
     // color: "#7A7A7E",
-    color: "#fff",
+    color: "#1C1C1D",
     fontWeight: "300",
     cursor: "pointer",
     "&:hover": {
@@ -35,117 +36,104 @@ const Footer: React.FC = () => {
     },
   }));
 
+  const NavbarLogo = styled("img")(({ theme }: { theme: Theme }) => ({
+    cursor: "pointer",
+    height: 70,
+    [theme.breakpoints.down("md")]: {
+      display: "none",
+    },
+  }));
+
   return (
-    <Box sx={{ py: 10, backgroundColor: "#33334d" }}>
+    <Box sx={{ py: 10, backgroundColor: "#f4f6f8" }}>
       <CustomContainer>
-        <CustomContainer>
-          <Box>
-            <Typography
-              sx={{
-                fontSize: "20px",
-                // color: "#1C1C1D",
-                color: "#fff",
-                fontWeight: "700",
-                mb: 2,
-              }}
-            >
-              Products
-            </Typography>
+        <Box>
+          <NavbarLogo src={logoImg} alt="logo" />
+          <Typography
+            sx={{
+              fontSize: "20px",
+              // color: "#1C1C1D",
+              color: "#1C1C1D",
+              fontWeight: "700",
+              mb: 2,
+            }}
+          >
+            Associate Management
+          </Typography>
+          <Typography
+            sx={{
+              fontSize: "16px",
+              // color: "#1C1C1D",
+              color: "#1C1C1D",
+              mb: 2,
+              maxWidth: 600,
+            }}
+          >
+            AD PREMIER, JL. TB Simatupang NO.5, RW.7, Ragunan, Kecamatan Pasar
+            Minggu, Kota Jakarta Selatan, Daerah Khusus Ibukota Jakarta 12550
+          </Typography>
 
-            <FooterLink>Listing</FooterLink>
-            <br />
-            <FooterLink>Properties</FooterLink>
-            <br />
-            <FooterLink>Agents</FooterLink>
-            <br />
-            <FooterLink>Blog</FooterLink>
-          </Box>
+          <IconBox>
+            <img src={fbIcon} alt="fbIcon" style={{ cursor: "pointer" }} />
+            <img
+              src={twitterIcon}
+              alt="twitterIcon"
+              style={{ cursor: "pointer" }}
+            />
+            <img
+              src={linkedinIcon}
+              alt="linkedinIcon"
+              style={{ cursor: "pointer" }}
+            />
+          </IconBox>
+        </Box>
 
-          <Box>
-            <Typography
-              sx={{
-                fontSize: "20px",
-                // color: "#1C1C1D",
-                color: "#fff",
+        <Box>
+          <Typography
+            sx={{
+              fontSize: "20px",
+              // color: "#1C1C1D",
+              color: "#1C1C1D",
 
-                fontWeight: "700",
-                mb: 2,
-              }}
-            >
-              Resources
-            </Typography>
+              fontWeight: "700",
+              mb: 2,
+            }}
+          >
+            Navigations
+          </Typography>
 
-            <FooterLink>Our Homes</FooterLink>
-            <br />
-            <FooterLink>Stories</FooterLink>
-            <br />
-            <FooterLink>Video</FooterLink>
-            <br />
-            <FooterLink>Free Trial</FooterLink>
-          </Box>
+          <FooterLink>Home</FooterLink>
+          <br />
+          <FooterLink>About</FooterLink>
+          <br />
+          <FooterLink>Services</FooterLink>
+          <br />
+          <FooterLink>Listed</FooterLink>
+          <br />
+          <FooterLink>Contact</FooterLink>
+        </Box>
 
-          <Box>
-            <Typography
-              sx={{
-                fontSize: "20px",
-                // color: "#1C1C1D",
-                color: "#fff",
-                fontWeight: "700",
-                mb: 2,
-              }}
-            >
-              Company
-            </Typography>
+        <Box>
+          <Typography
+            sx={{
+              fontSize: "20px",
+              // color: "#1C1C1D",
+              color: "#1C1C1D",
+              fontWeight: "700",
+              mb: 2,
+            }}
+          >
+            Company
+          </Typography>
 
-            <FooterLink>Partnerships</FooterLink>
-            <br />
-            <FooterLink>Terms of use</FooterLink>
-            <br />
-            <FooterLink>Privacy</FooterLink>
-            <br />
-            <FooterLink>Sitemap</FooterLink>
-          </Box>
-
-          <Box>
-            <Typography
-              sx={{
-                fontSize: "20px",
-                // color: "#1C1C1D",
-                fontWeight: "700",
-                color: "#fff",
-                mb: 2,
-              }}
-            >
-              Get in touch
-            </Typography>
-
-            <Typography
-              sx={{
-                fontSize: "16px",
-                // color: "#7A7A7E",
-                color:'#fff',
-                fontWeight: "500",
-                mb: 2,
-              }}
-            >
-              You’ll find your next home, in any style you prefer.
-            </Typography>
-
-            <IconBox>
-              <img src={fbIcon} alt="fbIcon" style={{ cursor: "pointer" }} />
-              <img
-                src={twitterIcon}
-                alt="twitterIcon"
-                style={{ cursor: "pointer" }}
-              />
-              <img
-                src={linkedinIcon}
-                alt="linkedinIcon"
-                style={{ cursor: "pointer" }}
-              />
-            </IconBox>
-          </Box>
-        </CustomContainer>
+          <FooterLink>Partnerships</FooterLink>
+          <br />
+          <FooterLink>Terms of use</FooterLink>
+          <br />
+          <FooterLink>Privacy</FooterLink>
+          <br />
+          <FooterLink>Sitemap</FooterLink>
+        </Box>
       </CustomContainer>
     </Box>
   );
