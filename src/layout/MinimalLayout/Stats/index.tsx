c524f051740ac9ce1df70af34e2f26960fa16611
ranisup97 <ranisup97@gmail.com>
2023-09-11 15:00:21 +0700

    add stats

--- src/layout/MinimalLayout/Stats/index.tsx
+++ src/layout/MinimalLayout/Stats/index.tsx
@@ -1,4 +1,4 @@
-import { Container, Grid, Typography, Box } from "@mui/material";
+import { Container, Grid, Typography, Box, Link } from "@mui/material";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faClipboardCheck,
@@ -6,6 +6,7 @@ import {
   faFileAlt,
   faClock,
 } from "@fortawesome/free-solid-svg-icons";
+import CustomButton from "../../../components/CustomButton/CustomButton";
 
 const StatsSection = () => {
   return (
@@ -18,6 +19,16 @@ const StatsSection = () => {
       }}
     >
       <Container>
+        <Box
+          sx={{
+            alignItems: "center",
+            paddingBottom: 6,
+          }}
+        >
+          <Typography sx={{ fontSize: 40, textAlign: "center" }}>
+            Helping 10,000+ associates.
+          </Typography>
+        </Box>
         <Grid container spacing={4}>
           <Grid item xs={12} sm={6} md={3}>
             <Box
@@ -65,7 +76,7 @@ const StatsSection = () => {
           <Grid item xs={12} sm={6} md={3}>
             <Box sx={{ display: "flex", py: 2, justifyContent: "center" }}>
               <FontAwesomeIcon icon={faClock} size="4x" className="mr-3" />
-              
+
               <Box sx={{ marginInlineStart: 1 }}>
                 <Typography variant="h2" gutterBottom>
                   8565
@@ -76,6 +87,30 @@ const StatsSection = () => {
           </Grid>
         </Grid>
       </Container>
+      <Box
+        sx={{
+          alignItems: "center",
+          alignContent: "center",
+          alignSelf: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Link
+          href="/register"
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            paddingTop: 6,
+          }}
+        >
+          <CustomButton
+            backgroundColor="white"
+            color="#33334d"
+            buttonText="Join our network"
+          />
+        </Link>
+      </Box>
     </Box>
   );
 };
