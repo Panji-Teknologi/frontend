5e94a4baab4f33733b5cc3c7d4c2e88e9b7dd3cd
panjiggm <panjiggm@gmail.com>
2023-09-15 16:59:30 +0700

    add chat wa

--- src/layout/MainLayout/index.tsx
+++ src/layout/MainLayout/index.tsx
@@ -4,7 +4,7 @@ import { useDispatch, useSelector } from 'react-redux';
 
 // material-ui
 import { useTheme } from '@mui/material/styles';
-import { Box, Toolbar, useMediaQuery } from '@mui/material';
+import { Box, IconButton, Slide, Toolbar, useMediaQuery, Link } from '@mui/material';
 
 // project import
 import Drawer from './Drawer';
@@ -15,12 +15,14 @@ import Breadcrumbs from '../../components/@extended/Breadcrumbs';
 // types
 import { openDrawer } from '../../store/reducers/menu';
 import Bottommenu from './BottomMenu';
+import MainCard from '../../components/MainCard';
+import { WhatsAppOutlined } from '@ant-design/icons';
 
 // ==============================|| MAIN LAYOUT ||============================== //
 
 const MainLayout = () => {
   const dispatch = useDispatch();
-  const theme = useTheme();
+  const theme = useTheme<any>();
   const matchDownLG = useMediaQuery(theme.breakpoints.down('lg'));
   const matchDownXS = useMediaQuery(theme.breakpoints.down(512));
 
@@ -53,6 +55,27 @@ const MainLayout = () => {
       <Box component="main" sx={{ width: '100%', flexGrow: 1, p: { xs: 2, sm: 3 } }}>
         <Toolbar />
         <Breadcrumbs navigation={navigation} title />
+        <Slide direction='right' in={true} mountOnEnter unmountOnExit>
+          <MainCard
+            sx={{
+              width: { xs: '100%', sm: 'auto' },
+              position: 'fixed',
+              zIndex: 9,
+              right: { xs: 0, sm: 16 },
+              bottom: { xs: 0, sm: '25%' },
+              borderRadius: 50,
+              p: 1
+            }}
+            content={false}
+            shadow={theme.customShadows.z1}
+            boxShadow
+            border={false}
+          >
+            <IconButton LinkComponent={Link} href='https://wa.me/6281218227597?text=Halo' target='_blank'>
+              <WhatsAppOutlined style={{ color: 'green', fontSize: 30 }} />
+            </IconButton>
+          </MainCard>
+        </Slide>
         <Outlet />
       </Box>
       {matchDownXS && <Bottommenu />}
