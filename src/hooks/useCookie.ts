ec7ec4cac30251f268a61e0916d2467dee079910
panjiggm <panjiggm@gmail.com>
2023-09-26 13:51:24 +0700

    change cookie to local storage

--- src/hooks/useCookie.ts
+++ src/hooks/useCookie.ts
@@ -1,24 +1,23 @@
 import { useState, useCallback } from "react";
-import Cookies from "js-cookie";
 
 const useCookie = (name: string, defaultValue?: any) => {
     const [value, setValue] = useState(() => {
-        const cookie = Cookies.get(name)
+        const cookie = window.localStorage.getItem(name)
         if (cookie) return cookie
-        Cookies.set(name, defaultValue)
+        window.localStorage.setItem(name, defaultValue)
         return defaultValue
     })
 
     const updateCookie = useCallback(
-        (newValue: any, options: any) => {
-            Cookies.set(name, newValue, options)
+        (newValue: any) => {
+            window.localStorage.setItem(name, newValue)
             setValue(newValue)
         },
         [name]
     )
 
     const deleteCookie = useCallback(() => {
-        Cookies.remove(name)
+        window.localStorage.removeItem(name)
         setValue(null)
     }, [name])
 
